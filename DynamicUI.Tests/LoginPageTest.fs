f98050e6d7b887aa1220e155a9f0408f4bd832fe
Edgar <edgar.gonzalezpena@aviva.com>
2019-12-31 12:21:31 +0100

    Remove unused Tests

--- DynamicUI.Tests/LoginPageTest.fs
+++ DynamicUI.Tests/LoginPageTest.fs
@@ -1,16 +1,4 @@
 module DynamicUI.Tests
 
-open NUnit.Framework
-open DynamicUI.LoginPage
-open FsUnit
 
-module ``LoginPage Tests`` =
-    [<Test>]
-    let ``Init should return a valid initial state``() =
-        let initialState =
-            { Email = "exmaple@email.com"
-              Password = "enter a password"
-              IsEmailValid = false
-              isPasswordValid = false }
 
-        LoginPage.init |> should equal initialState
